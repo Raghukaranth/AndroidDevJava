7f728ab68b39f3e925a3808f8f81507f69e0601c
raghavendra.s3 <raghavendrakaranth39@gmail.com>
2024-09-16 15:48:36 +0530

    Updated Gradel for Local Home for Mac and implemented Utils Interface for storing all the API's coming from backend

--- app/src/main/java/com/example/androiddevjava/utils/UtilsInterface.java
+++ app/src/main/java/com/example/androiddevjava/utils/UtilsInterface.java
@@ -1,2 +1,6 @@
-package com.example.androiddevjava.utils;public interface UtilsInterface {
+package com.example.androiddevjava.utils;
+
+public interface UtilsInterface {
+    String GET_ALL_EMPLOYEE = "/employee/get-all";
+    String SAVE_EMPLOYEE = "/employee/save";
 }

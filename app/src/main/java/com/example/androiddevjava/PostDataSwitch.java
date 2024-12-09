a30baa58cd377d5c932d2c95b3445316c2c0202e
RaghuKaranth <raghavendrakaranth39@gmail.com>
2024-12-09 18:28:01 +0530

    activity switch

---
+++ app/src/main/java/com/example/androiddevjava/PostDataSwitch.java
@@ -0,0 +1,31 @@
+package com.example.androiddevjava;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.androiddevjava.model.Data;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
+public class PostDataSwitch extends AppCompatActivity {
+    private TextView responseTV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_data_switch);
+
+        String userDataJson = getIntent().getStringExtra("user_data");
+        List<Data> Data = new Gson().fromJson(userDataJson, new TypeToken<List<Data>>(){}.getType());
+
+        responseTV = findViewById(R.id.idTVResponse);
+        responseTV.setText(Data.toString());
+    }
+}
